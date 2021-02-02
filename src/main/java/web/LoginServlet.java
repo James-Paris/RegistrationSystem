d5878a0e96133e84a614ec5f1c49ee4423a8cb99
James <James.Paris@Hcl.com>
2021-02-02 10:45:02 -0600

    Login handling

--- src/main/java/web/LoginServlet.java
+++ src/main/java/web/LoginServlet.java
@@ -2,12 +2,19 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.query.*;
+
+import hibernate.HibernateUtils;
+import model.UserEntity;
 
 /**
  * Servlet implementation class LoginServlet
@@ -47,9 +54,55 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		out.println("<h3>Loading...</h3>");
+		//out.println("<h3>Loading...</h3>");
         
-		doGet(req, resp);
+		String checkUser = req.getParameter("user");
+		String checkPass = req.getParameter("pass");
+		
+		Session session = HibernateUtils.buildSessionFactory().openSession();
+		session.beginTransaction();
+		
+		//check to see if user exists
+		Query query = session.createQuery("from UserEntity where username = :usr ", UserEntity.class);
+		query.setParameter("usr", checkUser);
+		
+		List results = query.list();
+		if(results.isEmpty()) {
+			//that user doesnt exist
+			out.println("<h3>That user does not exist.</h3>");
+			out.println("<p><b>Please <a href=''>Register</a> it if you would to login using this username.</b></p>");
+			doGet(req, resp);
+		} else {
+			//check to see if passwords match
+			UserEntity fetchedData = (UserEntity) results.get(0);
+			if(checkPass.equals(fetchedData.getPass())) {
+				//user exists and passwords match! Log the user in and redirect:
+				out.println("<h3>Login Successful! Redirecting...</h3>");
+				successfulLogin(req, resp);
+			} else {
+				out.println("<h3>Invalid login credentials. Please try again!</h3>");
+				doGet(req, resp);
+				//passwords did not match. handle that now
+			}
+			
+			
+			
+		}
+				
+		
+		//doGet(req, resp);
 	}
+	
+		protected void successfulLogin(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			
+			// Setting up the content type of web page
+	        resp.setContentType("text/html");
+	        // Writing the message on the web page
+	        PrintWriter out = resp.getWriter();
+			resp.setHeader("Refresh", "5; URL=http://localhost:8050/widgets/theforums");
+	    	
+			
+		}
+		
 
 }
